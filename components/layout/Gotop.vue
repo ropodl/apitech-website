c9ad194ebb74fcd165436efce24214fa9a85887a
ropodl <sarox14@gmail.com>
2023-04-13 18:43:22 +0545

    small animation tweak

--- components/layout/Gotop.vue
+++ components/layout/Gotop.vue
@@ -12,33 +12,20 @@ const onScroll = (e) => {
 </script>
 <template>
   <v-btn
-    aria-label="Go to top"
     icon
+    aria-label="Go to top button"
     size="x-large"
     color="primary"
     class="rounded-circle position-fixed"
     v-scroll="onScroll"
     :style="{
-      bottom: fab ? '20px' : '-100px',
+      bottom: fab ? '20px' : '-70px',
       right: '40px',
       zIndex: '999',
-      transition: 'all 250ms linear',
+      transition: 'all 250ms cubic-bezier(.17,.26,1,.32) 0s',
     }"
     @click="scrollTo('__nuxt')"
   >
     <v-icon :icon="mdiArrowUp"></v-icon>
   </v-btn>
 </template>
-<style>
-.slide-fade-enter-active {
-  transition: all 0.3s ease;
-}
-.slide-fade-leave-active {
-  transition: all 0.3s ease-in-out;
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateY(50px);
-  opacity: 0;
-}
-</style>
