27253aa37ca988ee387315ad3b6d0e866b3d22f7
ropodl <sarox14@gmail.com>
2023-04-28 22:57:11 +0545

    dynamic error page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  error: Object,
+});
+</script>
+<template>
+  <div>
+    <VitePwaManifest />
+    <NuxtLoadingIndicator />
+    <v-app>
+      <NuxtLayout>
+        <LazyLayoutPageSection
+          :title="error.statusCode + ' ' + error.statusMessage"
+        />
+      </NuxtLayout>
+    </v-app>
+  </div>
+</template>
