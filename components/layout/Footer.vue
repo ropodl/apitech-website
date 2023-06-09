6cc8097f8b21e62dd2ae75d89ead0ae0f55261e1
ropodl <sarox14@gmail.com>
2023-06-09 20:34:44 +0545

    time

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -116,7 +116,7 @@ const contactList = [
           <v-divider class="my-3"></v-divider>
           <div class="mb-3">
             <v-icon :icon="mdiClockOutline"></v-icon>
-            10 AM - 5 PM, Sunday - Friday
+            10 AM - 6 PM, Sunday - Friday
           </div>
           <div class="mb-3">Please feel free to visit us at given time.</div>
           <div class="text-h6 font-weight-bold">Special Partner</div>
