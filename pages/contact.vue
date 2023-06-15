1553018ed6a3413656597b5b3fc3a0c80e4ad157
ropodl <sarox14@gmail.com>
2023-06-15 20:15:47 +0545

    contact page email updated

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,119 +1,108 @@
 <script setup>
-import emailjs from "@emailjs/browser";
-import { mdiEmail, mdiMapMarker, mdiPhone } from "@mdi/js";
-import { useTheme } from "vuetify";
-const theme = useTheme();
+  import emailjs from "@emailjs/browser";
+  import { mdiEmail, mdiMapMarker, mdiPhone } from "@mdi/js";
+  import { useTheme } from "vuetify";
+  const theme = useTheme();
 
-useHead({
-  title: "API Technology - Contact Us for Services in Web, Mobile & More",
-  meta: [
+  useHead({
+    title: "API Technology - Contact Us for Services in Web, Mobile & More",
+    meta: [
+      {
+        name: "description",
+        content: "API Technology provides business to business services in Web, Mobile, Networking, ICT and Electronics Hardware areas.",
+      },
+    ],
+  });
+
+  const isDark = computed(() => (theme.global.name.value == "dark" ? true : false));
+
+  const contactForm = ref(null);
+  let loading = ref(false);
+  let snackbar = reactive({
+    show: false,
+    text: "",
+  });
+
+  const submitForm = async () => {
+    loading.value = true;
+    const { valid } = await contactForm.value.validate();
+    if (valid) {
+      emailjs
+        .send("service_3ujy4pd", "template_0wt2tnn", templateParams, "-xkHJckmH36raNsEo")
+        .then((response) => {
+          if (response.status == "200") {
+            snackbar["text"] = "Successfully sent, we will reply soon.";
+            snackbar["show"] = true;
+            contactForm.value.reset();
+          }
+        })
+        .catch((err) => {
+          console.log("FAILED...", err);
+        });
+    } else console.log("failed");
+    loading.value = false;
+  };
+
+  const templateParams = reactive({
+    from_name: "",
+    from_mobile_number: "",
+    from_email: "",
+    to_name: "API Technology",
+    from_subject: "",
+    message: "",
+  });
+
+  const rules = {
+    fullName: [
+      (value) => {
+        if (value?.length > 3) return true;
+        return "Full name must be more than 3 characters.";
+      },
+    ],
+    phoneNumber: [
+      (value) => {
+        if (value?.length > 6) return true;
+        return "Phone Number must be at least 7 characters.";
+      },
+    ],
+    emailAddress: [
+      (value) => {
+        if (value?.length > 3) return true;
+        return "Email Address must be at least 3 characters.";
+      },
+      (v) => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || "Email Address must be in a valid format",
+    ],
+    subject: [
+      (value) => {
+        if (value?.length > 3) return true;
+        return "Subject must be at least 3 characters.";
+      },
+    ],
+    message: [
+      (value) => {
+        if (value?.length > 3) return true;
+        return "Message must be at least 3 characters.";
+      },
+    ],
+  };
+
+  const infoList = [
     {
-      name: "description",
-      content:
-        "API Technology provides business to business services in Web, Mobile, Networking, ICT and Electronics Hardware areas.",
+      icon: mdiMapMarker,
+      title: "Contact Address",
+      content: "Mid Baneshwor",
     },
-  ],
-});
-
-const isDark = computed(() =>
-  theme.global.name.value == "dark" ? true : false
-);
-
-const contactForm = ref(null);
-let loading = ref(false);
-let snackbar = reactive({
-  show: false,
-  text: "",
-});
-
-const submitForm = async () => {
-  loading.value = true;
-  const { valid } = await contactForm.value.validate();
-  if (valid) {
-    emailjs
-      .send(
-        "service_3ujy4pd",
-        "template_0wt2tnn",
-        templateParams,
-        "-xkHJckmH36raNsEo"
-      )
-      .then((response) => {
-        if (response.status == "200") {
-          snackbar["text"] = "Successfully sent, we will reply soon.";
-          snackbar["show"] = true;
-          contactForm.value.reset();
-        }
-      })
-      .catch((err) => {
-        console.log("FAILED...", err);
-      });
-  } else console.log("failed");
-  loading.value = false;
-};
-
-const templateParams = reactive({
-  from_name: "",
-  from_mobile_number: "",
-  from_email: "",
-  to_name: "API Technology",
-  from_subject: "",
-  message: "",
-});
-
-const rules = {
-  fullName: [
-    (value) => {
-      if (value?.length > 3) return true;
-      return "Full name must be more than 3 characters.";
+    {
+      icon: mdiEmail,
+      title: "General Inquiry",
+      content: "info.apitech@gmail.com",
     },
-  ],
-  phoneNumber: [
-    (value) => {
-      if (value?.length > 6) return true;
-      return "Phone Number must be at least 7 characters.";
+    {
+      icon: mdiPhone,
+      title: "Phone Number",
+      content: "01-4472774 ",
     },
-  ],
-  emailAddress: [
-    (value) => {
-      if (value?.length > 3) return true;
-      return "Email Address must be at least 3 characters.";
-    },
-    (v) =>
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        v
-      ) || "Email Address must be in a valid format",
-  ],
-  subject: [
-    (value) => {
-      if (value?.length > 3) return true;
-      return "Subject must be at least 3 characters.";
-    },
-  ],
-  message: [
-    (value) => {
-      if (value?.length > 3) return true;
-      return "Message must be at least 3 characters.";
-    },
-  ],
-};
-
-const infoList = [
-  {
-    icon: mdiMapMarker,
-    title: "Contact Address",
-    content: "Mid Baneshwor",
-  },
-  {
-    icon: mdiEmail,
-    title: "General Inquiry",
-    content: "info@apitechnepal.com",
-  },
-  {
-    icon: mdiPhone,
-    title: "Phone Number",
-    content: "01-4472774 ",
-  }
-];
+  ];
 </script>
 <template>
   <LazyLayoutPageSection title="Contact Us" />
@@ -124,42 +113,14 @@ const infoList = [
           <v-card class="px-3 pt-4">
             <v-card-title class="mb-3">Send a message</v-card-title>
             <v-card-text class="pb-0">
-              <v-text-field
-                v-model="templateParams['from_name']"
-                :rules="rules['fullName']"
-                placeholder="Full Name"
-              ></v-text-field>
-              <v-text-field
-                v-model="templateParams['from_mobile_number']"
-                :rules="rules['phoneNumber']"
-                placeholder="Phone Number"
-              ></v-text-field>
-              <v-text-field
-                v-model="templateParams['from_email']"
-                :rules="rules['emailAddress']"
-                placeholder="Email Address"
-              ></v-text-field>
-              <v-text-field
-                v-model="templateParams['from_subject']"
-                :rules="rules['subject']"
-                placeholder="Subject"
-              ></v-text-field>
-              <v-textarea
-                v-model="templateParams['message']"
-                :rules="rules['message']"
-                placeholder="Message"
-              ></v-textarea>
+              <v-text-field v-model="templateParams['from_name']" :rules="rules['fullName']" placeholder="Full Name"></v-text-field>
+              <v-text-field v-model="templateParams['from_mobile_number']" :rules="rules['phoneNumber']" placeholder="Phone Number"></v-text-field>
+              <v-text-field v-model="templateParams['from_email']" :rules="rules['emailAddress']" placeholder="Email Address"></v-text-field>
+              <v-text-field v-model="templateParams['from_subject']" :rules="rules['subject']" placeholder="Subject"></v-text-field>
+              <v-textarea v-model="templateParams['message']" :rules="rules['message']" placeholder="Message"></v-textarea>
             </v-card-text>
             <v-card-text class="pt-0">
-              <v-btn
-                block
-                type="submit"
-                size="large"
-                rounded="sm"
-                variant="flat"
-                class="text-capitalize"
-                >Submit</v-btn
-              >
+              <v-btn block type="submit" size="large" rounded="sm" variant="flat" class="text-capitalize">Submit</v-btn>
             </v-card-text>
           </v-card>
         </v-form>
@@ -170,13 +131,7 @@ const infoList = [
           Please<span class="primary--text"> get in touch.</span>
         </div>
         <v-row class="mb-3">
-          <v-col
-            v-for="(info, i) in infoList"
-            :key="i"
-            cols="12"
-            md="12"
-            lg="6"
-          >
+          <v-col v-for="(info, i) in infoList" :key="i" cols="12" md="12" lg="6">
             <v-list rounded="xl" class="elevation-1">
               <v-list-item>
                 <template #prepend>
@@ -185,9 +140,7 @@ const infoList = [
                   </v-avatar>
                 </template>
                 <v-list-item-title v-text="info['title']"></v-list-item-title>
-                <v-list-item-subtitle
-                  v-text="info['content']"
-                ></v-list-item-subtitle>
+                <v-list-item-subtitle v-text="info['content']"></v-list-item-subtitle>
               </v-list-item>
             </v-list>
             <!-- <div class="text-overline">{{ info.title }}</div>
@@ -236,14 +189,7 @@ const infoList = [
   <v-snackbar v-model="snackbar['show']" theme="light">
     {{ snackbar["text"] }}
     <template v-slot:actions>
-      <v-btn
-        class="text-capitalize px-4"
-        color="primary"
-        variant="tonal"
-        @click="snackbar['show'] = false"
-      >
-        Close
-      </v-btn>
+      <v-btn class="text-capitalize px-4" color="primary" variant="tonal" @click="snackbar['show'] = false"> Close </v-btn>
     </template>
   </v-snackbar>
 </template>
